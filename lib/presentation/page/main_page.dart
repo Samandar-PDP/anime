dc9d9519d9b9bdc3e806dae034398969d152183f
roziqov1 <rozivoyroziqov@gmail.com>
2024-04-27 14:40:27 +0300

    ---

--- lib/presentation/page/main_page.dart
+++ lib/presentation/page/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/presentation/page/home_page.dart';
 import 'package:flutter_application_1/presentation/widget/bottom_tab.dart';
 
 class MainPage extends StatefulWidget {
@@ -10,9 +11,16 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   int _selectedIndex = 0;
+  final _pages = [
+    const HomePage(),
+    Container(),
+    Container(),
+    Container(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _pages[_selectedIndex],
       bottomNavigationBar: MyBottomTab(
         selectedIndex: _selectedIndex,
         onClick: (index) => setState(() { _selectedIndex = index; }),
